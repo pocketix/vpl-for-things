a5e5f52f90dd9de811f805b0b7bbf67e6a93ab41
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2024-02-25 23:12:04 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
 import { defineConfig } from 'vite';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'vpl-for-things',
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'VPLForThings',
+      fileName: 'vpl-for-things',
     },
     rollupOptions: {
-      external: ['lit'],
+      external: ['lit', 'monaco-editor'],
       output: {
         globals: {
-          lit: 'lit',
+          lit: 'Lit',
+          'monaco-editor': 'Monaco',
         },
       },
+    },
+  },
+  plugins: [dts()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/editor/components'),
+      '@vpl': path.resolve(__dirname, './src/vpl'),
     },
   },
 });
