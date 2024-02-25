4f6af483c4774e9c2885fa69798fd5ceab31f589
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2024-02-25 23:13:05 +0100

    Change include paths

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-export * from './editor/components/graphical-editor';
-export * from './vpl/base.language';
-export * from './vpl/language';
-export * from './vpl/program';
-export * from './vpl/test';
+import './index.css';
+
+export * from '@components/vpl-editor';
+export * from '@components/graphical-editor';
+export * from '@components/text-editor';
+export * from '@components/editor-controls';
+export * from '@components/ge-statement';
+export * from '@components/ge-block';
+export * from '@vpl/base.language';
+export * from '@vpl/language';
+export * from '@vpl/program';
