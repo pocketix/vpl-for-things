90cac93dd6b467b941bb09c01ba45baaf1ccfc57
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2024-03-31 22:26:39 +0200

    Register new components

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ export * from '@components/text-editor';
 export * from '@components/editor-controls';
 export * from '@components/ge-statement';
 export * from '@components/ge-block';
+export * from '@components/editor-modal';
+export * from '@components/editor-button';
+export * from '@components/editor-expression-list';
+export * from '@components/editor-icon';
+export * from '@components/editor-expression-modal';
+export * from '@components/ge-statement-argument';
+
 export * from '@vpl/base.language';
 export * from '@vpl/language';
 export * from '@vpl/program';
