c515652d8d6e582262bb0bf43c1a435a0fd0c7a1
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2024-04-06 21:28:15 +0200

    Expand base language

--- src/vpl/base.language.ts
+++ src/vpl/base.language.ts
@@ -1,53 +1,141 @@
 import { Statements } from './language';
 
 export const baseLanguageStatements: Statements = {
+  // Logic
   if: {
     type: 'compound_with_args',
+    group: 'logic',
     label: 'If',
-    icon: '',
-    foregroundColor: '',
-    backgroundColor: '',
-    args: [{ type: 'bool_expr' }],
+    icon: 'diagram2',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#3b82f6',
+    args: [{ type: 'bool_expr', label: 'Condition' }],
+  },
+  elseif: {
+    type: 'compound_with_args',
+    group: 'logic',
+    label: 'Else If',
+    icon: 'diagram2',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#3b82f6',
+    args: [{ type: 'bool_expr', label: 'Condition' }],
+    predecessors: ['if'],
   },
   else: {
     type: 'compound',
+    group: 'logic',
     label: 'Else',
-    icon: '',
-    foregroundColor: '',
-    backgroundColor: '',
-    predecessors: ['if'],
+    icon: 'diagram2',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#3b82f6',
+    predecessors: ['if', 'elseif'],
   },
+  switch: {
+    type: 'compound_with_args',
+    group: 'logic',
+    label: 'Switch',
+    icon: 'diagram3',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#3b82f6',
+    nestedStatements: ['case'],
+    args: [{ type: 'str' }],
+  },
+  case: {
+    type: 'compound_with_args',
+    group: 'logic',
+    label: 'Case',
+    icon: 'diagram3',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#3b82f6',
+    args: [{ type: 'str' }],
+  },
+
+  // Loops
   repeat: {
     type: 'compound_with_args',
+    group: 'loop',
     label: 'Repeat',
-    icon: '',
-    foregroundColor: '',
-    backgroundColor: '',
-    args: [{ type: 'num' }],
+    icon: 'repeat1',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#10b981',
+    args: [{ type: 'num', label: 'Number of Repetitions' }],
   },
+  while: {
+    type: 'compound_with_args',
+    group: 'loop',
+    label: 'While',
+    icon: 'repeat',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#10b981',
+    args: [{ type: 'bool_expr', label: 'Condition' }],
+  },
+
+  // IoT Specific
   alert: {
     type: 'unit_with_args',
+    group: 'iot',
     label: 'Send Notification',
-    icon: '',
-    foregroundColor: '',
-    backgroundColor: '',
+    icon: 'bell',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#8b5cf6',
     args: [
       {
         type: 'str_opt',
+        label: 'Notification Method',
         options: [
           { id: 'phone_number', label: 'Phone Number' },
           { id: 'email', label: 'Email' },
         ],
       },
-      { type: 'str' },
+      { type: 'str', label: 'Email or Phone Number' },
     ],
   },
+
+  // Variables
   setvar: {
     type: 'unit_with_args',
+    group: 'variable',
     label: 'Set Variable',
-    icon: '',
-    foregroundColor: '',
-    backgroundColor: '',
-    args: [{ type: 'var' }, { type: 'unknown' }],
+    icon: 'bracesAsterisk',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#d97706',
+    args: [
+      { type: 'var', label: 'Variable' },
+      { type: 'unknown', label: 'Value' },
+    ],
+  },
+
+  teststmt: {
+    type: 'compound_with_args',
+    group: 'misc',
+    label: 'Testing Statement',
+    icon: 'lightningChargeFill',
+    foregroundColor: '#ffffff',
+    backgroundColor: '#078706',
+    args: [
+      { type: 'bool', label: 'IsTurnedOn' },
+      { type: 'bool_expr', label: 'IdkU' },
+      { type: 'bool_expr', label: 'aaaaaaaaa' },
+      { type: 'num', label: 'asdsdfsdijbf' },
+      { type: 'num_expr', label: 'sdfhsdf' },
+      {
+        type: 'num_opt',
+        label: 'sdhibisd',
+        options: [
+          { id: 10, label: '10' },
+          { id: 20, label: '20' },
+        ],
+      },
+      { type: 'str', label: 'asdfij' },
+      {
+        type: 'str_opt',
+        label: 'ojsijndf',
+        options: [
+          { id: 'str_opt1', label: 'Str Opt 1' },
+          { id: 'str_opt2', label: 'Str Opt 2' },
+        ],
+      },
+      { type: 'unknown' },
+    ],
   },
 };
