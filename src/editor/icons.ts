340cd6ed1b6495852bf0bca835effefc74c5ba3d
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2024-04-12 10:05:09 +0200

    Add list menu icon

--- src/editor/icons.ts
+++ src/editor/icons.ts
@@ -441,3 +441,17 @@ export const checkLg = html`
       d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z" />
   </svg>
 `;
+
+export const list = html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    class="bi bi-list"
+    viewBox="0 0 16 16">
+    <path
+      fill-rule="evenodd"
+      d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+  </svg>
+`;
