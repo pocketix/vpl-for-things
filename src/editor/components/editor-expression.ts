a768f2904a929ea78d4787ec99e1ef52b840f132
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2024-04-14 23:47:53 +0200

    Refactor

--- src/editor/components/editor-expression.ts
+++ src/editor/components/editor-expression.ts
@@ -1,17 +1,12 @@
-import { Expression, Language, Program, compareOperators } from '@/index';
-import { consume } from '@lit/context';
+import { EditorExpressionOperandList, Expression, Program } from '@/index';
 import { LitElement, html, css, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { languageContext, programContext } from '../context/editor-context';
-import {
-  editorExpressionCustomEvent,
-  expressionListCustomEvent,
-  graphicalEditorCustomEvent,
-} from '../editor-custom-events';
-import { globalStyles } from '../global-styles';
+import { editorExpressionCustomEvent } from '../editor-custom-events';
 import { Ref, createRef, ref } from 'lit/directives/ref.js';
-import { v4 as uuidv4 } from 'uuid';
-import { plusLg, xLg } from '../icons';
+import { globalStyles } from '../global-styles';
+import * as icons from '@/editor/icons';
+import { consume } from '@lit/context';
+import { programContext } from '../context/editor-context';
 
 @customElement('editor-expression')
 export class EditorExpression extends LitElement {
@@ -20,119 +15,159 @@ export class EditorExpression extends LitElement {
     css`
       :host {
         display: block;
-        border-radius: 0.5rem;
-      }
-
-      .expressions-wrapper {
-        display: flex;
-        align-items: center;
-      }
-
-      .expr-header-wrapper {
-        display: flex;
-        position: sticky;
-        justify-content: end;
-        align-items: center;
-        background: white;
-        padding: 0.5rem;
-        padding-top: 0.75rem;
-        padding-bottom: 0.75rem;
-        /* border-radius: 0.5rem; */
-        box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
-        cursor: pointer;
+        width: 100%;
       }
 
       .expr-wrapper {
-        position: relative;
-        background: white;
-        /* border-radius: 0.5rem; */
-      }
-
-      .group-relation {
-        font-weight: 500;
-        font-family: var(--mono-font);
         width: 100%;
-        text-align: end;
+        box-sizing: border-box;
       }
 
-      .grouped-by-or {
-        background-color: #f9731633;
+      .indented {
+        border-left: 1px solid var(--gray-400);
       }
 
-      .grouped-by-and {
-        background-color: #3b82f633;
+      .expr-selected .opr-button {
+        font-weight: 600;
       }
 
-      .grouped-by-and-header {
-        background-color: #60a5fa;
+      .grouped-by-and .opr-button {
+        color: var(--blue-500);
       }
 
-      .grouped-by-or-header {
-        background-color: #fb923c;
+      .grouped-by-or .opr-button {
+        color: var(--orange-500);
       }
 
-      .expr-view {
+      .grouped-by-other {
+      }
+
+      .opr-button-wrapper {
+        display: flex;
+        flex-direction: column;
+        position: sticky;
+        top: 0;
+        cursor: pointer;
+        user-select: none;
+        z-index: 400;
+      }
+
+      .opr-button {
+        background: rgb(255, 255, 255);
+        padding: 4px 2px;
+        font-size: 1.25rem;
+        line-height: 1rem;
         font-family: var(--mono-font);
-        text-align: center;
-        overflow-x: auto;
       }
 
-      .expr-select-checkbox {
-        margin-left: 0.5rem !important;
+      .dots {
+        border-left: 1px solid var(--gray-400);
+        margin-left: 7px;
+        padding-left: 4px;
       }
 
-      .operator-select {
-        padding-top: 0.5rem;
-        padding-bottom: 0.5rem;
-        font-family: var(--mono-font);
+      .bottom-line {
+        padding-bottom: 10px;
+        border-bottom: 1px solid var(--gray-400);
+        border-bottom-left-radius: 8px;
+      }
+
+      .grouped-by-and .expr-opd-list {
+        border-color: var(--blue-500);
+        background-color: rgba(59, 130, 246, 0.05);
+      }
+
+      .grouped-by-or .expr-opd-list {
+        border-color: var(--orange-500);
+        background-color: rgba(249, 115, 22, 0.05);
+      }
+
+      .expr-selected .expr-opd-list {
+        border-width: 3px;
+      }
+
+      .grouped-by-other .expr-opd-list {
+        background-color: rgba(107, 114, 128, 0.05);
+      }
+
+      .and-group {
+        color: var(--blue-500);
+      }
+
+      .or-group {
+        color: var(--orange-500);
+      }
+
+      .indent-wrapper {
+        display: flex;
+      }
+
+      .indent {
+        height: 100%;
+        width: 8px;
       }
     `,
   ];
 
-  @property() expr: Expression;
-  @property() index: number;
-  @property() exprIsSelected: boolean = false;
-  @property() nestedLevel: number;
+  @property() expression: Expression;
+  @property() nestedExprListIsVisible: boolean = true;
+  @property() nestedLevel: number = 0;
+  @property() exprIsSelected: boolean;
   @property() highlightedExpr: HTMLElement;
-  @property() exprListIsSelected: boolean;
-
-  @consume({ context: languageContext })
-  @property()
-  language?: Language;
 
   @consume({ context: programContext })
   @property()
   program?: Program;
 
-  expressionGroupWrapperRef: Ref<HTMLElement> = createRef();
-  exprHeaderWrapperRef: Ref<HTMLElement> = createRef();
   exprWrapperRef: Ref<HTMLElement> = createRef();
+  nestedExprListWrapperRef: Ref<EditorExpressionOperandList> = createRef();
 
-  handleOprChange(event: Event, expr: Expression) {
-    if (this.program.operatorIsUnary(event.currentTarget.value) && !this.program.operatorIsUnary(expr.opr)) {
-      delete expr.opd1;
+  handleOprButtonClicked(e: Event) {
+    if (!this.nestedExprListIsVisible) {
+      this.handleHighlightList(e);
     }
-    if (this.program.operatorIsUnary(expr.opr) && !this.program.operatorIsUnary(event.currentTarget.value)) {
-      expr.opd1 = { type: 'unknown', value: null };
-    }
-    expr.opr = event.currentTarget.value;
 
-    const evnt = new CustomEvent(graphicalEditorCustomEvent.PROGRAM_UPDATED, {
-      bubbles: true,
-      composed: true,
-    });
-    this.dispatchEvent(evnt);
+    if (
+      this.nestedExprListIsVisible &&
+      this.highlightedExpr &&
+      this.exprWrapperRef.value &&
+      this.highlightedExpr === this.exprWrapperRef.value
+    ) {
+      this.handleHighlightList(e);
+    }
+
+    if (
+      this.nestedExprListIsVisible &&
+      this.highlightedExpr &&
+      this.exprWrapperRef.value &&
+      this.highlightedExpr !== this.exprWrapperRef.value
+    ) {
+      this.handleHighlightList(e);
+      return;
+    }
+
+    if (this.highlightedExpr === undefined && this.nestedExprListIsVisible) {
+      this.handleHighlightList(e);
+      return;
+    }
+
+    if (this.nestedExprListIsVisible) {
+      this.handleHideNestedExprList();
+    } else {
+      this.handleShowNestedExprList();
+    }
   }
 
-  handleSelectExpression(index: number) {
-    this.exprIsSelected = !this.exprIsSelected;
+  handleShowNestedExprList() {
+    this.nestedExprListWrapperRef.value.classList.add('block');
+    this.nestedExprListWrapperRef.value.classList.remove('hidden');
+    this.nestedExprListIsVisible = true;
+  }
 
-    const evnt = new CustomEvent(editorExpressionCustomEvent.EXPRESSION_SELECTED, {
-      bubbles: true,
-      composed: true,
-      detail: { index: index, selectedExpr: this.expr },
-    });
-    this.dispatchEvent(evnt);
+  handleHideNestedExprList() {
+    this.nestedExprListWrapperRef.value.classList.add('hidden');
+    this.nestedExprListWrapperRef.value.classList.remove('block');
+    this.nestedExprListIsVisible = false;
   }
 
   handleHighlightList(e: Event) {
@@ -141,8 +176,7 @@ export class EditorExpression extends LitElement {
       composed: true,
       detail: {
         exprToHighlight: this.exprWrapperRef.value,
-        exprHeaderToHighlight: this.exprHeaderWrapperRef.value,
-        exprGroupRelation: this.expr.opr,
+        exprGroupRelation: this.expression.opr,
       },
     });
     this.dispatchEvent(evnt);
@@ -150,87 +184,48 @@ export class EditorExpression extends LitElement {
     e.stopPropagation();
   }
 
-  expressionModifyTemplate() {
-    return html`
-      <div class="expressions-wrapper" style="width: 100%;">
-        ${!this.program.operatorIsUnary(this.expr.opr)
-          ? html` <editor-expression-operand .operand="${this.expr.opd1}"></editor-expression-operand> `
-          : nothing}
-        <select
-          .value="${this.expr.opr}"
-          @change="${(e) => this.handleOprChange(e, this.expr)}"
-          class="operator-select"
-          name="">
-          ${compareOperators.map((exprOpt) => {
-            return html`<option value="${exprOpt}" ?selected=${this.expr.opr === exprOpt}>${exprOpt}</option>`;
-          })}
-        </select>
-        <editor-expression-operand .operand="${this.expr.opd2}"></editor-expression-operand>
-        ${this.exprListIsSelected || this.nestedLevel === 1
-          ? html`
-              <input
-                type="checkbox"
-                class="expr-select-checkbox"
-                .value="${this.exprIsSelected}"
-                @change="${() => this.handleSelectExpression(this.index)}" />
-            `
-          : nothing}
-      </div>
-    `;
-  }
-
-  expressionViewTemplate() {
-    return html` <div class="expr-view">${this.program.parseGroupedExpressions(this.expr)}</div> `;
-  }
-
   render() {
     return html`
-      ${this.expr.exprList
-        ? html`
-            <div
-              ${ref(this.exprWrapperRef)}
-              class="expr-wrapper"
-              style="${this.expr.opr === '&&'
-                ? 'border: 2px solid #3b82f6; border-top: none;'
-                : this.expr.opr === '||'
-                ? 'border: 2px solid #f97316; border-top: none;'
-                : 'border: 2px solid var(--gray-300); border-top: none;'}">
+      <div ${ref(this.exprWrapperRef)} class="expr-wrapper">
+        ${this.expression.opr
+          ? html`
               <div
-                ${ref(this.exprHeaderWrapperRef)}
-                class="expr-header-wrapper"
-                style="top: ${this.nestedLevel * 47}px; z-index: ${2000 - this.nestedLevel * 4 + 6}; ${this.expr.opr ===
-                '&&'
-                  ? 'border-top: 2px solid #3b82f6;'
-                  : this.expr.opr === '||'
-                  ? 'border-top: 2px solid #f97316;'
-                  : 'border-top: 2px solid var(--gray-300); justify-content: center;'}">
-                ${this.expr.opr !== '??'
-                  ? html`<div class="group-relation" @click="${this.handleHighlightList}">${this.expr.opr}</div>`
-                  : html`<div class="group-relation" style="opacity: 0;">a</div>`}
-                ${this.exprListIsSelected || this.nestedLevel === 1
-                  ? html`
-                      <input
-                        type="checkbox"
-                        class="expr-select-checkbox"
-                        .value="${this.exprIsSelected}"
-                        @change="${() => this.handleSelectExpression(this.index)}" />
-                    `
-                  : nothing}
+                @click="${this.handleOprButtonClicked}"
+                class="opr-button-wrapper"
+                style="${`top: ${(this.nestedLevel - 1) * 23}px; z-index: ${300 - (this.nestedLevel + 4)}`}">
+                <div
+                  class="opr-button ${this.expression.opr === '&&'
+                    ? 'and-group'
+                    : this.expression.opr === '||'
+                    ? 'or-group'
+                    : ''}">
+                  ${this.program.convertOprToDisplayOpr(this.expression.opr)}
+                </div>
+                <div class="dots ${this.nestedExprListIsVisible ? 'hidden' : ''}">
+                  <editor-icon .icon="${icons.threeDots}"></editor-icon>
+                </div>
               </div>
-
-              <editor-expression-list
-                .exprList="${this.expr.exprList}"
-                .nestedLevel="${this.nestedLevel + 1}"
-                .highlightedExpr="${this.highlightedExpr}"
-                .exprIsSelected="${this.highlightedExpr &&
-                this.exprWrapperRef.value &&
-                this.highlightedExpr === this.exprWrapperRef.value}">
-              </editor-expression-list>
-            </div>
-          `
-        : html`${this.exprListIsSelected || this.nestedLevel === 1
-            ? this.expressionModifyTemplate()
-            : this.expressionViewTemplate()}`}
+            `
+          : nothing}
+        <div class="indent-wrapper">
+          ${this.nestedLevel > 0 ? html`<div class="indent"></div>` : nothing}
+          <editor-expression-operand-list
+            ${ref(this.nestedExprListWrapperRef)}
+            .parentExpr="${this.expression}"
+            .operands="${this.expression.opds}"
+            .nestedLevel="${this.nestedLevel}"
+            .highlightedExpr="${this.highlightedExpr}"
+            .exprIsSelected="${this.highlightedExpr &&
+            this.exprWrapperRef.value &&
+            this.highlightedExpr === this.exprWrapperRef.value}"
+            class="expr-opd-list ${this.expression.opr ? 'indented' : ''} ${!this.nestedExprListIsVisible &&
+            this.nestedLevel > 0
+              ? 'hidden'
+              : 'block'} ${this.nestedLevel > 0 ? 'bottom-line' : ''}"
+            style="${this.nestedLevel === 0 ? 'padding-bottom: 108px;' : ''}">
+          </editor-expression-operand-list>
+        </div>
+      </div>
     `;
   }
 }
