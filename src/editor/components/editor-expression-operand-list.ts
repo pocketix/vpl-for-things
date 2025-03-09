aec9353ad2064f7f9201d4866fc662751469ee05
Petr John <ijohn@fit.vut.cz>
2025-03-09 10:16:32 +0100

    Feature #5: Fix the implicit top Expression block in the expression editor can be modified - its type can be changed

--- src/editor/components/editor-expression-operand-list.ts
+++ src/editor/components/editor-expression-operand-list.ts
@@ -365,7 +365,7 @@ export class EditorExpressionOperandList extends LitElement {
   render() {
     return html`
       <div class="expr-opd-list-wrapper">
-        ${this.operands.length < 1
+        ${!this.operands || this.operands.length < 1
           ? html`
               <div class="no-opd-message" style="${this.nestedLevel > 0 ? 'padding-top: 10px;' : ''}">
                 <div>Click on "+ Add Operand"</div>
@@ -492,7 +492,7 @@ export class EditorExpressionOperandList extends LitElement {
                         <editor-button
                           class="expr-control-button"
                           @click="${this.handleEnterGroupMode}"
-                          ?disabled="${this.operands.length < 1}">
+                          ?disabled="${this.operands?.length < 1}">
                           <editor-icon .icon="${check2square}"></editor-icon>
                           <span>Select ...</span>
                         </editor-button>
