e7afd18acd6498ce0d66a51578ed5884405c2052
Filip Štolfa <phillipstolfa@gmail.com>
2025-03-24 22:54:00 +0100

    feat: add runningBlock and isRunning contexts

--- src/editor/context/editor-context.ts
+++ src/editor/context/editor-context.ts
@@ -3,3 +3,6 @@ import { Language } from '@vpl/language';
 import { Block, Program } from '@vpl/program';
 export const languageContext = createContext<Language>(Symbol('language'));
 export const programContext = createContext<Program>(Symbol('program'));
+
+export const runninBlockContext = createContext<string>(Symbol('running-block')); // uuid of current running block
+export const isRunningContext = createContext<boolean>(Symbol('is-running'));
