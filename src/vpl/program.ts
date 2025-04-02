bb7f1627bdbb4c493797bbe7568d8fd0f17aee84
IvanIV100 <ivan.iv.chodak@gmail.com>
2025-04-02 20:19:18 +0200

    Added: Meta data  type

--- src/vpl/program.ts
+++ src/vpl/program.ts
@@ -144,6 +144,12 @@ export function assignUuidToBlock(block: Block) {
   }
 }
 
+export type MetadataInit = {
+  uuid: string;
+  id: string;
+  devices: string[];
+};
+
 export class Program {
   header: Header;
   block: Block;
@@ -152,8 +158,9 @@ export class Program {
     this.header = {
       userVariables: {},
       userProcedures: {},
+      initializedProcedures: [] as MetadataInit[], // Update initializedProcedures to use MetadataInit type
       skeletonize: [],
-      skeletonize_uuid: [], // Add skeletonize_uuid array to the header
+      skeletonize_uuid: [],
       selected_uuids: [],
     };
     this.block = [];
@@ -378,8 +385,9 @@ export type Header = {
   userProcedures: {
     [id: string]: Block;
   };
+  initializedProcedures: MetadataInit[]; // Update initializedProcedures to use MetadataInit type
   skeletonize: [];
-  skeletonize_uuid: string[]; // Add skeletonize_uuid array to the Header type
+  skeletonize_uuid: string[];
   selected_uuids: string[];
 };
 
