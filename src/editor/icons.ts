b23db5efa29a4f19cbcffadac85324af4cc43402
Filip Štolfa <phillipstolfa@gmail.com>
2025-05-14 23:29:50 +0200

    chore: add valve icon

--- src/editor/icons.ts
+++ src/editor/icons.ts
@@ -721,4 +721,9 @@ export const variableIcon = html`
 `;
 
 
-
+//mdi/valve (https://pictogrammers.com/library/mdi/icon/valve/)
+export const valveHalfWay = html`
+    <svg stroke="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path fill="currentColor" d="M4 22H2V2H4M22 2H20V22H22M17.24 5.34L13.24 9.34A3 3 0 0 0 9.24 13.34L5.24 17.34L6.66 18.76L10.66 14.76A3 3 0 0 0 14.66 10.76L18.66 6.76Z"></path>
+    </svg>
+`;
