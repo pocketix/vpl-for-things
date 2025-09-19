25ddab03091e7223de246c6320131d406c764588
Chodak Ivan <Ivan.iv.chodak@gmail.com>
2025-09-19 09:10:09 +0200

    FIxed - nested src file + reordered package.json

--- src/src/editor/context/editor-context.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from '@lit/context';
-import { Language } from '@vpl/language';
-import { Block, Program } from '@vpl/program';
-export const languageContext = createContext<Language>(Symbol('language'));
-export const programContext = createContext<Program>(Symbol('program'));
